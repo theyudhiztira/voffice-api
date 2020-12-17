664889349508e86fb4b01a5fa5aef53d66357cfa
Pandu Yudhistira <pandu.y@unionspace.com>
2020-12-17 17:21:48 +0700

    Added Auth Checker for Partnership

---
+++ middlewares/partnership/auth.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try{
+        const decodedToken = jwt.verify(req.headers.authorization.split(" ")[1], process.env.JWT_KEY)
+        req.userData = decodedToken;
+        next();
+    }catch(error){
+        return res.status(403).send({
+            message: 'Auth Failed'
+        });
+    }
+}
