40ccee6a92a34811eb2fa1a9bcbe6938c1e9900c
BayuRidhoAlkhifli <bayuridhoalkhifli@gmail.com>
2021-05-31 11:42:59 +0700

    Create: Client Plan migration

--- migrations/20210527033021-create-plan.js
+++ migrations/20210527033021-create-plan.js
@@ -9,30 +9,34 @@ module.exports = {
                 type: Sequelize.INTEGER
             },
             company_id: Sequelize.INTEGER,
+            product_id: Sequelize.INTEGER,
+            current_price: Sequelize.DOUBLE,
+            renewal_price: Sequelize.INTEGER,
             location_id: Sequelize.INTEGER,
             credit: Sequelize.INTEGER,
             start_date: {
-              allowNull: false,
-              type: Sequelize.DATE
+                allowNull: false,
+                type: Sequelize.DATE
             },
             last_renew_date: {
-              allowNull: false,
-              type: Sequelize.DATE
+                allowNull: true,
+                type: Sequelize.DATE
             },
             next_renew_date: {
-              allowNull: false,
-              type: Sequelize.DATE
+                allowNull: false,
+                type: Sequelize.DATE
             },
             contract_term: Sequelize.INTEGER,
             billing_cycle: Sequelize.INTEGER,
             extend_period: Sequelize.INTEGER,
             assigned_phone_no: Sequelize.STRING,
             assigned_fax_no: Sequelize.STRING,
-            domicile_number: Sequelize.INTEGER,
+            domicile_number: Sequelize.STRING,
             status: {
-              type: Sequelize.INTEGER,
-              comment: "0 = Unpaid, 1 = Notified Paid. 2 = Checked & Paid, 3 = Activated, 4 = Expired, 5 = Activation Pending, 6 = Cancelled"
+                type: Sequelize.INTEGER,
+                comment: "0 = Unpaid, 1 = Notified Paid. 2 = Checked & Paid, 3 = Activated, 4 = Expired, 5 = Activation Pending, 6 = Cancelled"
             },
+            created_by: Sequelize.INTEGER,
             created_at: {
                 allowNull: false,
                 type: Sequelize.DATE
